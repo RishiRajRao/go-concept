f179810b04e0ea06583c86c0dd8460c951e13336
“Rishi” <rishiraj.rao@tablt.com>
2024-06-18 22:16:35 +0530

    mutex

---
+++ goroutineslikepromise.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"sync"
+	"time"
+)
+
+type ToDos struct {
+	UserID    int    `json:"userId"`
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
+func maintt() {
+
+	urls := []string{"https://jsonplaceholder.typicode.com/todos/1", "https://jsonplaceholder.typicode.com/todos/2", "https://jsonplaceholder.typicode.com/todos/3"}
+	var (
+		wg     sync.WaitGroup
+		result []interface{}
+	)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	ch := make(chan interface{})
+	wg.Add(len(urls))
+
+	for _, url := range urls {
+		go GetTodos(ctx, url, ch, &wg)
+	}
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	for val := range ch {
+		var todo ToDos
+		if err := json.Unmarshal(val.([]byte), &todo); err != nil {
+			fmt.Printf("Error unmarshalling JSON: %v\n", err)
+			continue
+		}
+		result = append(result, todo)
+	}
+
+	fmt.Println("final result ", result)
+
+	defer cancel()
+
+}
+
+func GetTodos(ctx context.Context, url string, ch chan<- interface{}, wg *sync.WaitGroup) {
+	defer wg.Done()
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	if err != nil {
+		ch <- err
+		return
+		// return nil, fmt.Errorf("failed to create request: %w", err)
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		ch <- err
+		return
+		// return nil, fmt.Errorf("failed to get response: %w", err)
+	}
+	defer resp.Body.Close()
+
+	// body, err := io.ReadAll(resp.Body)
+	// if err != nil {
+	// 	return nil, fmt.Errorf("failed to read response body: %w", err)
+	// }
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		ch <- err
+		return
+	}
+
+	ch <- body
+
+}
--- mutex.go
+++ mutex.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-func main() {
+func maindy() {
 	n := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
 	ch := make(chan struct{})
 	var repoMutex sync.Mutex // To protect the repo slice
